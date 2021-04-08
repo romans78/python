5c8055489b0d8f41272d1fec6527da0c5ae66901
romans78 <34432686+romans78@users.noreply.github.com>
2021-04-07 23:18:56 -0400

    Delete hw2 directory

--- hw2/ex1.py
+++
@@ -1,3 +0,0 @@
-data_list = [1, True, 1.0, "string", None, (2, 3), {4, 5}, [0, "p"], 1 + 1j, {1: "1", 2: "2"}, b"bytes"]
-for data in data_list:
-    print(data, ": type -", type(data))
--- hw2/ex5.py
+++
@@ -1,9 +0,0 @@
-lst = [125, 62, 33, 33, 7, 7, 7, 5]
-print("Текущий рейтинг:", lst)
-
-new_element = int(input("Введите новый элемент рейтинга: "))
-
-lst.append(new_element)
-lst.sort(reverse=True)
-
-print("Новый рейтинг:", lst)
