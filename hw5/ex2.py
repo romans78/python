46d53e2221ec6c6b7e061941349da086f745bc4d
romans78 <34432686+romans78@users.noreply.github.com>
2021-04-07 23:19:21 -0400

    Delete hw5 directory

--- hw5/ex2.py
+++
@@ -1,8 +0,0 @@
-with open("file.txt", encoding="utf-8") as in_file:
-    lines = in_file.readlines()
-    n_lines = len(lines)
-    n_words_in_lines = map(lambda l: len(l.split()), lines)
-    print(f"Файл содержит {n_lines} строк")
-    print("Количество слов в каждой строке:")
-    print(*list(map(lambda l: "строка " + str(l[0]) + ": " + str(l[1]), enumerate(n_words_in_lines, start=1))),
-          sep="\n")
--- hw5/ex5.py
+++
@@ -1,6 +0,0 @@
-numbers = "1 76 32 43.5 456 32 44 -17.5 -0.7 1.234"
-with open("numbers.txt", "w") as nums:
-    nums.write(numbers)
-
-with open("numbers.txt") as nums:
-    print(f"Сумма чисел в файле равна {sum(map(float, nums.readline().split()))}")
