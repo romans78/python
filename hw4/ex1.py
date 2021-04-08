6c51b72cba7d9c76b447b40030fca19195f77d21
romans78 <34432686+romans78@users.noreply.github.com>
2021-04-07 23:19:12 -0400

    Delete hw4 directory

--- hw4/ex1.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-
-def calculate_salary(hours, hour_rate, bonus):
-    return hours * hour_rate + bonus
-
-
-hours, hour_rate, bonus = tuple(map(float, argv[1:]))
-print(f"Заработная плата составляет {calculate_salary(hours, hour_rate, bonus):.2f}")
--- hw4/ex5.py
+++
@@ -1,4 +0,0 @@
-from functools import reduce
-
-
-print(reduce(lambda x, y: x * y, [n for n in range(100, 1001, 2)]))
